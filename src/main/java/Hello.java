fa7f4acbc0bdaf42edefde9f6c12747b94e53d84
xjh <xjh@123.com>
2022-05-16 19:27:21 +0800

    commit first

---
+++ src/main/java/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * @author xjh
+ * @className Hello
+ * @description
+ * @date 2022/5/16 19:20
+ * version 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
